d6d8a9329892149106601ffe8ecb885219a286fb
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-25 10:11:50 +0530

    splash screen completed

--- lib/splash.dart
+++ lib/splash.dart
@@ -10,6 +10,27 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(),
+          Image.asset('assets/Group 61.png'),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 80),
+            child: Text(
+              'BLUG',
+              style: TextStyle(
+                color: Color(0xFF525FE1),
+                fontSize: 36,
+                fontFamily: 'Alumni Sans',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

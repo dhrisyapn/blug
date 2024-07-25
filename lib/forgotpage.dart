de9ecea6c22920f3cce47001672b877acd896fb7
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-25 13:49:40 +0530

    content added to forgot page

--- lib/forgotpage.dart
+++ lib/forgotpage.dart
@@ -10,6 +10,139 @@ class ForgotPaasswordPage extends StatefulWidget {
 class _ForgotPaasswordPageState extends State<ForgotPaasswordPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+            ),
+            Image.asset('assets/Group 12.png'),
+            SizedBox(
+              height: 60,
+            ),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Get a link to reset \nour ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 45,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'secret',
+                    style: TextStyle(
+                      color: Color(0xFFFF6B00),
+                      fontSize: 45,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' into \nyour ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 45,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'mail',
+                    style: TextStyle(
+                      color: Color(0xFFFF6B00),
+                      fontSize: 45,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' box!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 45,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, bottom: 60),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email address',
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 18,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Your awesome email here',
+                      hintStyle: TextStyle(
+                          color: Color(0x7F525FE1),
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w100,
+                          fontSize: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                    ),
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 17,
+                    ),
+                    cursorColor: Color(0xFF525FE1),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 45,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFFF6B00),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Get Link',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

91e787346af2318cab8b4a8eb24c3cdc8248c07d
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-25 12:45:32 +0530

    content added to signup page

--- lib/signup.dart
+++ lib/signup.dart
@@ -10,6 +10,199 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+            ),
+            Image.asset('assets/Group 12.png'),
+            SizedBox(
+              height: 70,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email address',
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 18,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Your awesome email here',
+                      hintStyle: TextStyle(
+                          color: Color(0x7F525FE1),
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w100,
+                          fontSize: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                    ),
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 17,
+                    ),
+                    cursorColor: Color(0xFF525FE1),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Full Name',
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 18,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Your splendid name here ',
+                      hintStyle: TextStyle(
+                          color: Color(0x7F525FE1),
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w100,
+                          fontSize: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                    ),
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 17,
+                    ),
+                    cursorColor: Color(0xFF525FE1),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'New Password',
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 18,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Secret here',
+                      hintStyle: TextStyle(
+                          color: Color(0x7F525FE1),
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w100,
+                          fontSize: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                    ),
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 17,
+                    ),
+                    cursorColor: Color(0xFF525FE1),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Re-type Password',
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 18,
+                      fontFamily: 'Alumni Sans',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Secret again here',
+                      hintStyle: TextStyle(
+                          color: Color(0x7F525FE1),
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w100,
+                          fontSize: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide:
+                            BorderSide(width: 1, color: Color(0xFF525FE1)),
+                      ),
+                    ),
+                    style: TextStyle(
+                      color: Color(0xFF525FE1),
+                      fontSize: 17,
+                    ),
+                    cursorColor: Color(0xFF525FE1),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 45,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFFF6B00),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Create account',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontFamily: 'Alumni Sans',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
